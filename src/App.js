4e0c105ef4afb5e9b0929193ac6c93f27cfcbf40
Aite Aigbe <aite@nabla.ooo>
2022-03-22 02:28:24 -0400

    glimb

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 
+import Webcam from 'react-webcam';
+import styled from 'styled-components'
+import { useRef } from 'react';
+
+const Camera = styled.div`
+position: absolute;
+top: 0;
+right: 0;
+width: 100vw;
+height: 100vh;
+`
+
+
 function App() {
+  const cameraRef = useRef(null);
   return (
     <div className="App">
       {/* TODO: Add webcam for debugging */}
       hello world
+      <Camera ref={cameraRef}>
+        <Webcam />
+      </Camera>
     </div>
   );
 }
