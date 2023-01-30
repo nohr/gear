2f75266a15de19c76c0d26ed95f5e5ea37fd7187
Aite Aigbe <aite@nabla.ooo>
2023-01-30 20:48:11 +0100

    start camera on start

--- src/components/ML/Cameras.tsx
+++ src/components/ML/Cameras.tsx
@@ -1,22 +1,28 @@
 import Webcam from "react-webcam";
 import { state } from "state";
+import { useSnapshot } from "valtio";
 import { activateDraw } from "./utils";
 
 export default function Cameras() {
+  const { cameraStarted } = useSnapshot(state);
   return (
     <>
-      {/* MediaPipe Camera */}
-      <Webcam width={640} height={480} className="input_video" />
-      {/* Handtrack js Camera */}
-      <Webcam
-        width={640}
-        height={480}
-        className="input_video2 "
-        onLoadedData={() => {
-          state.cameraLoaded = true;
-        }}
-      />
-      <canvas className="guides" ref={(e: any) => activateDraw(e)}></canvas>
+      {cameraStarted ? (
+        <>
+          {/* MediaPipe Camera */}
+          <Webcam width={640} height={480} className="input_video" />
+          {/* Handtrack js Camera */}
+          <Webcam
+            width={640}
+            height={480}
+            className="input_video2 "
+            onLoadedData={() => {
+              state.cameraLoaded = true;
+            }}
+          />
+          <canvas className="guides" ref={(e: any) => activateDraw(e)}></canvas>
+        </>
+      ) : null}
     </>
   );
 }
